53e47547079f0ccc6b8e9f9a7f39cd33b2d1b00d
Lukáš Rataj <l9aiagency@gmail.com>
2026-04-03 12:22:26 +0200

    feat: PWA manifest + icons for add-to-homescreen

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,11 @@ export default function RootLayout({
   return (
     <html lang="cs" className={`${montserrat.variable} ${inter.variable}`}>
       <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#6B21A8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
